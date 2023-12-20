e2124f204848892c149f09b999ae654ae7dc5523
Nora Liu <noraliu0830@gmail.com>
2023-12-20 22:25:50 +0800

    feat: add countdown to debate

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Debate from "./page/Debate";
+import ProgressBar from "./components/ProgressBar";
+import Countdown from "./components/Countdown";
 
 function App() {
-  return <Debate />;
+  return (
+    <>
+      <Debate />
+      {/* <ProgressBar /> */}
+      <Countdown />
+    </>
+  );
 }
 
 export default App;
