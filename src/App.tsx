27fab53af8be2db0bd591b0db96d4d2b72076752
Nora Liu <noraliu0830@gmail.com>
2023-12-22 16:29:02 +0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Debate from "./page/Debate";
-import ProgressBar from "./components/ProgressBar";
 import Countdown from "./components/Countdown";
 
 function App() {
   return (
     <>
       <Debate />
-      {/* <ProgressBar /> */}
       <Countdown />
     </>
   );
