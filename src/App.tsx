60a31467e9b709ecdcf79ec2090241b28c551a91
Nora Liu <noraliu0830@gmail.com>
2023-12-24 15:12:35 +0800

    fix: update main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Debate from "./page/Debate";
-import FullScreenVideoAll from "./components/FullScreenVideo";
-import TopLeftBox from "./components/TopLeftBox";
 
 function App() {
   return (
     <>
-      {/* <Debate /> */}
-      <TopLeftBox />
-      <FullScreenVideoAll />
+      <Debate />
     </>
   );
 }
