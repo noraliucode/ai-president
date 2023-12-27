3e2ea2c190f16a732d9f26e3fadbfe3e604919a9
Nora Liu <noraliu0830@gmail.com>
2023-12-28 02:17:56 +0800

    feat: add randomly play video

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Debate from "./page/Debate";
+import FullScreenVideo from "./components/FullScreenVideo";
 
 function App() {
   return (
     <>
+      <FullScreenVideo />
       <Debate />
     </>
   );
--- src/components/FullScreenVideo.tsx
+++ src/components/FullScreenVideo.tsx
@@ -1,6 +1,14 @@
-import React from "react";
-// import myVideo from '/ai-president-01.mp4'; // Replace with your video file path
+import React, { useState, useEffect } from "react";
 import { styled } from "@mui/material/styles";
+
+const videos = [
+  "/video1.mp4",
+  "/video2.mp4",
+  "/video3.mp4",
+  "/video4.mp4",
+  "/video5.mp4",
+  "/video6.mp4",
+];
 
 const FullScreenVideo = styled("video")({
   position: "fixed",
@@ -10,19 +18,41 @@ const FullScreenVideo = styled("video")({
   height: "100%",
   objectFit: "cover",
   transform: "translate(-50%, -50%)",
-  zIndex: "-1",
+  zIndex: "3000",
 });
 
-const FullScreenVideoAll: React.FC = () => {
+const App = () => {
+  const [videoSrc, setVideoSrc] = useState("");
+  const [key, setKey] = useState(0); // Unique key for the video component
+
+  useEffect(() => {
+    selectRandomVideo();
+  }, []);
+
+  const selectRandomVideo = () => {
+    const randomIndex = Math.floor(Math.random() * videos.length);
+    console.log(
+      `Selecting video at index ${randomIndex}: ${videos[randomIndex]}`
+    );
+    setVideoSrc(videos[randomIndex]);
+    setKey((prevKey) => prevKey + 1); // Update key to force re-render
+  };
+
   return (
     <div className="App">
-      <FullScreenVideo autoPlay loop>
-        <source src={"/ai-president-01.mp4"} type="video/mp4" />
-        Your browser does not support the video tag.
-      </FullScreenVideo>
-      {/* Your other content goes here */}
+      {videoSrc && (
+        <FullScreenVideo
+          autoPlay
+          loop={false}
+          onEnded={selectRandomVideo}
+          key={key} // Unique key to force re-render
+        >
+          <source src={videoSrc} type="video/mp4" />
+          Your browser does not support the video tag.
+        </FullScreenVideo>
+      )}
     </div>
   );
 };
 
-export default FullScreenVideoAll;
+export default App;
